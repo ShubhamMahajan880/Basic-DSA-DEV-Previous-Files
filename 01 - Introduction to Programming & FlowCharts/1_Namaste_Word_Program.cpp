96e222854ee05c3673c2a62dcabcdfc5469f1086
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2025-05-23 12:34:44 +0530

    Add files via upload

---
+++ 01 - Introduction to Programming & FlowCharts/1_Namaste_Word_Program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<< "Namaste Shubham, How are you doing :-)";// It will print w/o enter or new line charcater 
+    cout<< "Namaste Shubham, How are you doing :-)"<<endl;// By using "<<endl" we will get enter or cursor in new line 
+    cout<< "Namaste Shubham, How are you doing :-)"<<'\n';// \n is also a new line charcter and uses for same use 
+    cout<< "Namaste Shubham, How are you doing :-)\n";// Another method for writting in a new line character
+    cout<< "Namaste Shubham, How are you doing :-)";
+    cout<<"\n";
+
+}
